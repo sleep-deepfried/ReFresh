3dacf08b0cb3b210993e5e766155f0b6f725c56b
sleep-deepfried <pulidoearljohn14@gmail.com>
2024-12-03 06:34:14 +0800

    addded signout buttpn

--- components/main/header.tsx
+++ components/main/header.tsx
@@ -13,36 +13,36 @@ export default async function Header() {
 
 
     // Fallback image and name if no session
-    const userImage = session?.user?.image || "/assets/default-user.png";
-    const userName = session?.user?.name || "Guest";
+    const userImage = session?.user?.image || "/assets/user.png";
+    const userName = session?.user?.name?.split(" ")[0] || "Guest";
 
     return (
         <div className="py-4 px-7 rounded-b-3xl bg-[#e0e0e0] shadow-[3px_3px_3px_3px_#9f9f9f,-3px_-3px_3px_3px_#ffffff]">
             <div className="flex flex-row justify-between items-center">
                 <div className="flex flex-row space-x-4">
-                    <div className="rounded-full overflow-hidden">
-                        <Image 
-                            src={userImage} 
-                            alt="Profile" 
-                            width={50} 
-                            height={50} 
-                            className="object-cover"
-                        />
-                    </div>
+                    <form
+                        action={async () => {
+                            "use server"
+                            await signOut()
+                            window.location.href = "/sign-in"
+                        }}
+                    >
+                        <button className="rounded-full overflow-hidden"
+                        type="submit"
+                        >
+                            <Image 
+                                src={userImage} 
+                                alt="Profile" 
+                                width={50} 
+                                height={50} 
+                                className="object-cover"
+                            />
+                        </button>
+                    </form>
                     <div>
                         <p>Hey, {userName}!</p>
                         <p>How&apos;s your diet today</p> 
                     </div>
-
-                    <form
-                        action={async () => {
-                            "use server"
-                            await signOut();
-                            window.location.href = "localhost:3000/sign-in";
-                        }}
-                        >
-                        <button type="submit">Sign Out</button>
-                    </form>
                 </div>
 
                 <IoIosNotifications className="text-4xl" />
